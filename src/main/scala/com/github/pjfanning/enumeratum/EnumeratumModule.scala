dbf85647ffe52ab4ebb7f9923102303e78a7428f
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-10 11:44:04 +0200

    Update EnumeratumModule.scala

--- src/main/scala/com/github/pjfanning/enumeratum/EnumeratumModule.scala
+++ src/main/scala/com/github/pjfanning/enumeratum/EnumeratumModule.scala
@@ -3,8 +3,8 @@ package com.github.pjfanning.enumeratum
 import com.github.pjfanning.enumeratum.deser.EnumeratumDeserializerModule
 import com.github.pjfanning.enumeratum.ser.EnumeratumSerializerModule
 
-class EnumeratumModule extends EnumeratumSerializerModule with EnumeratumDeserializerModule
-
-object EnumeratumModule extends EnumeratumModule {
+class EnumeratumModule extends EnumeratumSerializerModule with EnumeratumDeserializerModule {
   override def getModuleName: String = "EnumeratumModule"
 }
+
+object EnumeratumModule extends EnumeratumModule
